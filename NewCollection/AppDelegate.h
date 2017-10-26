330ba94136be471ca556c7b9664ffc827c9c5e7f
Marshal <921871381@qq.com>
2017-10-26 16:10:11 +0800

    Initial Commit

---
+++ NewCollection/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  NewCollection
+//
+//  Created by Marshal on 2017/10/26.
+//  Copyright © 2017年 Marshal. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ NewCollection/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  NewCollection
+//
+//  Created by Marshal on 2017/10/26.
+//  Copyright © 2017年 Marshal. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
